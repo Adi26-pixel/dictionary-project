19607e690d35bf7990c32f3c27eb7fcd09b9b3c0
Adeline Monsengo <59971657+Adi26-pixel@users.noreply.github.com>
2025-07-18 21:09:44 +0200

    Added a Dictionary search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import books from "./Books.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={books} alt="books" className="App-logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={books} alt="books" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Adeline Monsengo</small>
+        </footer>
+      </div>
     </div>
   );
 }
