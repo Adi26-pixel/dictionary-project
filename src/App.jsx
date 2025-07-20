7ee9353af5f0e2f456cf4b167edb092ea3593ca1
Adeline Monsengo <59971657+Adi26-pixel@users.noreply.github.com>
2025-07-20 23:31:45 +0200

    Updated the header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,20 @@ function App() {
       <div className="container">
         <header className="App-header">
           <img src={books} alt="books" className="App-logo img-fluid" />
+          <h1 className="App-heading">Dictionary App</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="hello" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Adeline Monsengo</small>
+          <small>
+            Coded by{" "}
+            <a href="https://github.com/Adi26-pixel">Adeline Monsengo</a> and
+            Opened source on{" "}
+            <a href="https://github.com/Adi26-pixel/dictionary-project.git">
+              Github
+            </a>
+          </small>
         </footer>
       </div>
     </div>
